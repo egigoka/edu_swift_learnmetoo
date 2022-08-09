6d1f2e170fa602e393001cf0b4ed5d66699b8f0a
Egor Egorov <egigoka@gmail.com>
2022-08-09 22:53:59 +0600

    cource 1 lesson 4 theory done partially

--- course1lesson4/Lesson 4.playground/Pages/Словари.xcplaygroundpage/Contents.swift
+++ course1lesson4/Lesson 4.playground/Pages/Словари.xcplaygroundpage/Contents.swift
@@ -6,13 +6,16 @@ import Foundation
 //: > Неупорядоченные коллекции, которые хранят в себе пары «ключ-значение». Тип ключа словаря может отличаться от типа значения. При этом сами ключи и значения должны быть одного типа
 
 //Полная форма записи словаря
+var someStringDictionary = Dictionary<String, String>()
+var moreStringDictionary: Dictionary<String, String> = [:]
 
 // Сокращенная форма записи словаря
+var anotherSomeDictionary = [Int: String]()
 
-
+var carWashQueue: [String: String] = [:]
 
 // Инициализация словаря с данными
-
+carWashQueue = ["E095AB": "Red camry"]
 
 // Изменение значения для ключа
 
