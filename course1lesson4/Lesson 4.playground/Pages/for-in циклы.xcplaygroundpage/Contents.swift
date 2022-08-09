2f33d2e9dac16af09f636e93e25014a5d9984b25
Egor Egorov <egigoka@gmail.com>
2022-08-09 22:08:54 +0600

    cource 1 lesson 4 theory done partially

---
+++ course1lesson4/Lesson 4.playground/Pages/for-in циклы.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: ## For-in циклы
+
+/*:
+ 
+    for counter in lower…upper {
+        some code
+    }
+ 
+ */
+
+
+
+//: ### Итерация по массиву
+
+
+//: ### Итерация по словарю
+
+
+
+
+//: ### Итерация по тексту
+
+
+
+
+//: [Next](@next)
---
+++ course1lesson4/Lesson 4.playground/Pages/while циклы.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: ## While циклы
+
+/*:
+ 
+    while condition {
+        some code
+    }
+ 
+ */
+
+
+
+
+
+
+/*:
+ 
+    repeat {
+        some code
+    } while condition
+ 
+ */
+
+
+//: [Next](@next)
