b7c4d4130def80028c0d1e4438baab9cff961b28
Egor Egorov <egigoka@gmail.com>
2022-08-10 22:38:36 +0600

    cource 1 lesson 4 theory done partially

--- course1lesson4/Lesson 4.playground/Pages/for-in циклы.xcplaygroundpage/Contents.swift
+++ course1lesson4/Lesson 4.playground/Pages/for-in циклы.xcplaygroundpage/Contents.swift
@@ -12,17 +12,28 @@ import Foundation
  
  */
 
+var sum = 0
 
 for counter in 1...5 {
-    
-    print(counter)
+    sum += counter
 }
 
+print(sum)
 //: ### Итерация по массиву
 
-let fruits = ["apple", "banana", "pineapple", "apple", "pear", "pear", "plum", "apricot", "apple"]
+let fruits = ["apple", "banana", "pineapple",
+              "apple", "pear", "apple",
+              "plum", "apricot", "apple"]
 
-print("The number of ")
+var apple = 0
+
+for fruit in fruits {
+    if fruit == "apple" {
+        apple += 1
+    }
+}
+
+print("The number of apples is \(apple) pieces")
 
 //: ### Итерация по словарю
 
