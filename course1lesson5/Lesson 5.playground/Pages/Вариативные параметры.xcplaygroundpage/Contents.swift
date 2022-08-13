49ea674ffb03f75caaf5846eb58e99508a961ba2
Egor Egorov <egigoka@gmail.com>
2022-08-13 23:31:30 +0600

    cource 1 lesson 5 theory done partially

--- course1lesson5/Lesson 5.playground/Pages/Вариативные параметры.xcplaygroundpage/Contents.swift
+++ course1lesson5/Lesson 5.playground/Pages/Вариативные параметры.xcplaygroundpage/Contents.swift
@@ -4,6 +4,16 @@ import Foundation
 
 //: ### Вариативные параметры
 
+func arithmeticMean(_ numbers: Double...) -> Double {
+    var total = 0.0
+    
+    for number in numbers {
+        total += number
+    }
+    
+    return total / Double(numbers.count)
+}
 
+arithmeticMean(4, 3.14, 7, 90, 56, 3)
 
 //: [Next](@next)
