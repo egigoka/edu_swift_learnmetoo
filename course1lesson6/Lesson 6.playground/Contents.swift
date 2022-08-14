0e6dcd5690be1f2649fef51a2f9e1b2e4e8ea9c3
Egor Egorov <egigoka@gmail.com>
2022-08-14 22:36:06 +0600

    cource 1 lesson 6 theory done partially

--- course1lesson6/Lesson 6.playground/Contents.swift
+++ course1lesson6/Lesson 6.playground/Contents.swift
@@ -3,6 +3,27 @@ import UIKit
 //: # Lesson 6
 //: ## Классы
 
+let postTitle = "Classes"
+let postBody = "Text of the article"
+let postAuthor = "Alexey Efimov"
+
+let postTitle2 = "Properties of classes"
+let postBody2 = "Text of the article"
+let postAuthor2 = postAuthor
+
+class Post {
+    var title = ""
+    var body = ""
+    var author = ""
+}
+
+let firstPost = Post()
+
+firstPost.title = "This is my first post"
+firstPost.body = "Hello"
+firstPost.author = "Efimov Alexey"
+
+print("Author \(firstPost.author) has published new post '\(firstPost.title)' with text '\(firstPost.body)'")
 
 
 //: ### Методы классов
