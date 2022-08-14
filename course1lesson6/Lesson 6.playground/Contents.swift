0e0f0a55c1c163de515890fbe5483014bb380733
Egor Egorov <egigoka@gmail.com>
2022-08-14 22:59:48 +0600

    cource 1 lesson 6 theory done partially

--- course1lesson6/Lesson 6.playground/Contents.swift
+++ course1lesson6/Lesson 6.playground/Contents.swift
@@ -15,6 +15,13 @@ class Post {
     var title = ""
     var body = ""
     var author = ""
+    var numberOfComments = 0
+    
+    func addComent() {
+        numberOfComments += 1
+    }
+    
+    
 }
 
 let firstPost = Post()
@@ -22,14 +29,47 @@ let firstPost = Post()
 firstPost.title = "This is my first post"
 firstPost.body = "Hello"
 firstPost.author = "Efimov Alexey"
+firstPost.addComent()
+firstPost.addComent()
+
+//let secondPost = firstPost
+let secondPost = Post()
+
+secondPost.title = "Second post"
+secondPost.body = "A lot of text"
+secondPost.author
 
 print("Author \(firstPost.author) has published new post '\(firstPost.title)' with text '\(firstPost.body)'")
+print("The number of comments for the post '\(firstPost.title)' is \(firstPost.numberOfComments)")
 
+print()
+
+print("Author \(secondPost.author) has published new post '\(secondPost.title)' with text '\(secondPost.body)'")
+print("The number of comments for the post '\(secondPost.title)' is \(secondPost.numberOfComments)")
+
+
+firstPost === secondPost
 
 //: ### Методы классов
 
 
+
 //: ### Инициализация
+
+class Human {
+    var name = ""
+    var age = 0
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+let somePerson = Human(name: "Ron", age: 24)
+
+somePerson.name
+somePerson.age
 
 
 //: ### Наследовение
