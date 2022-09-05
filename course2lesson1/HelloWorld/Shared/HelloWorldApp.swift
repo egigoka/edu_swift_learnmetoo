081016ef96582a420df4a23944056118e270ba1d
Egor Egorov <egigoka@gmail.com>
2022-09-06 00:11:46 +0600

    course 2 lesson 1 theory done partially

---
+++ course2lesson1/HelloWorld/Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by egigoka on 5/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
