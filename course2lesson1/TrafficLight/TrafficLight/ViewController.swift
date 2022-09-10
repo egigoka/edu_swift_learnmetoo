afa6a9b64176ac5d12ef709e5e926bcac4a344bb
Egor Egorov <egigoka@gmail.com>
2022-09-10 20:23:32 +0600

    course 2 lesson 1 homework done partially (TrafficLight partially done)

--- course2lesson1/TrafficLight/TrafficLight/ViewController.swift
+++ course2lesson1/TrafficLight/TrafficLight/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
+enum Status {
+    case uninitialized
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet var startNextButton: UIButton!
+    
+    var status = Status.uninitialized
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startNextButton.layer.cornerRadius = 10
+        
+        for view: UIView in [redSignalView, yellowSignalView, greenSignalView] {
+            view.layer.cornerRadius = view.bounds.height / 2
+        }
     }
 
-
+    @IBAction func startNextButtonPressed() {
+        startNextButton.setTitle("NEXT", for: .normal)
+        
+        switch status{
+        case .uninitialized:
+            redSignalView.alpha = 0.3
+            yellowSignalView.alpha = 0.3
+            greenSignalView.alpha = 0.3
+            fallthrough
+        case .green:
+            status = .red
+            greenSignalView.alpha = 0.3
+            redSignalView.alpha = 1
+            break
+        case .red:
+            status = .yellow
+            redSignalView.alpha = 0.3
+            yellowSignalView.alpha = 1
+            break
+        case .yellow:
+            status = .green
+            yellowSignalView.alpha = 0.3
+            greenSignalView.alpha = 1
+            break
+        }
+    }
+    
 }
 
