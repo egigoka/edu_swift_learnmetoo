0d68a49dd62827ed7a3bf470f0bba6b10e15b758
Egor Egorov <egigoka@gmail.com>
2022-09-17 22:19:39 +0600

    course 2 lesson 2 homework done partially (ColorPicker)

---
+++ course2lesson2/ColorPicker/ColorPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by egigoka on 17/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorView.rou
+    }
+
+
+}
+
