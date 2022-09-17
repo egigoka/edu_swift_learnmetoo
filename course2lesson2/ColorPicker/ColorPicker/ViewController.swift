4d3127e0d5f4d889c63a593932fed1ccdf1839a8
Egor Egorov <egigoka@gmail.com>
2022-09-17 23:09:17 +0600

    course 2 lesson 2 homework done (ColorPicker)

--- course2lesson2/ColorPicker/ColorPicker/ViewController.swift
+++ course2lesson2/ColorPicker/ColorPicker/ViewController.swift
@@ -13,13 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet var redLabel: UILabel!
     @IBOutlet var greenLabel: UILabel!
     @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colorView.rou
+        colorView.layer.cornerRadius = 10
+        colorView.layer.borderWidth = 1
+        colorView.layer.masksToBounds = true
+        colorView.layer.borderColor = UIColor.label.cgColor
     }
-
-
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        colorView.layer.borderColor = UIColor.label.cgColor
+    }
+    
+    @IBAction func sliderAction() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
 }
 
