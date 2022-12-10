ef25165a9c05b7c5833e228b152e4049576e3b33
Egor Egorov <egigoka@gmail.com>
2022-12-10 17:35:49 +0600

    course 2 lesson 4 theory done

---
+++ course2lesson4/AboutMe/AboutMe/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by egigoka on 10/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
