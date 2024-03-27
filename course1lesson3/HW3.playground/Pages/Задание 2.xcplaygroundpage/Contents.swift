b28b5227b4e65ee5ae13faef9642425dd0bb2d34
Egor Egorov <egigoka@gmail.com>
2024-03-27 18:42:53 +0500

    course 1 lesson 3 homework

--- course1lesson3/HW3.playground/Pages/Задание 2.xcplaygroundpage/Contents.swift
+++ course1lesson3/HW3.playground/Pages/Задание 2.xcplaygroundpage/Contents.swift
@@ -11,13 +11,20 @@ import Foundation
  Объявите переменную `userInputAge` типа `String` и присвойте ей значение "34e" для моделирования опечатки.  Затем определите константу `userAge` типа `Int` и используйте конструктор `Int` для преобразования `userInputAge`.  Какую ошибку вы получаете?
  */
 
+var userInputAge = "34e"
+//var userAge = 0
 
+//userAge = Int(userInputAge)
+// cannot assign optional Int value to an Int variable
 
 /*:
  Теперь поменяйте тип `userAge` на `Int?`, и напечатайте значение `userAge`.  Почему значение `userAge` равно `nil`?  Ответьте в комментарии или внутри команды print.
  */
 
+var userAge = Int(userInputAge)
 
+print(userAge)
+// not all characters are numbers, so it fallbacks to nil
 
 /*:
  Теперь исправьте опечатку в значении `userInputAge`.  Что-нибудь в результатах вывода кажется вам неуместным?
@@ -25,18 +32,31 @@ import Foundation
  Напечатайте `userAge` снова, но теперь извлеките `userAge` с помощью оператора «!».
  */
 
+userInputAge = "34"
 
+userAge = Int(userInputAge)
+
+print(userAge)
+
+print(userAge!)
 
 /*:
  Используйте привязку опционалов для извлечения `userAge`.  Если `userAge` содержит значение, выведите его в консоль.
  */
 
-
+if let userAge = userAge {
+    print("User entered age of \(userAge)")
+} else {
+    print("User entered age is in wrong format")
+}
 
 /*:
  Теперь снова эмулируйте опечатку и попробуйте вывести на консоль целочисленное значение переменной `userInputAge`, при помощи оператора "!". Какую ошибку вы получите? Почему?
  */
 
+userInputAge = "34e"
 
+//print(Int(userInputAge)!)
+// cannot forcefully unwrap nil because it doesn't contain value
 
 //: [Ранее: Задание 1](@previous)  |  задание 2 из 4  |  [Далее: Задание 3](@next)
