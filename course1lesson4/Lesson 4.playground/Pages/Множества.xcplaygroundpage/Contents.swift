9b410131dd26ed3f9aee7045c3d36e846328fa44
Egor Egorov <egigoka@gmail.com>
2024-05-08 04:28:18 +0500

    course 1 lesson 4 theory

--- course1lesson4/Lesson 4.playground/Pages/Множества.xcplaygroundpage/Contents.swift
+++ course1lesson4/Lesson 4.playground/Pages/Множества.xcplaygroundpage/Contents.swift
@@ -7,22 +7,26 @@ import Foundation
 
 // Создание пустого множества
 
-var charactes: Set<Character>
+let charactes: Set<Character> = []
+
+let integers = Set<Int>()
 
 // Создание множества с типом стринг
 
-
+var strings: Set = ["a", "a", "a", "b", "c"]
 
 // Добавление нового элемента в множество
 
+strings.insert("d")
 
+strings
 
 // Проверка на наличие определенного элемента во множестве
 
-
+strings.contains("b")
 
 // Сортировка с использованием метода sorted() для последующей итерации по множеству
 
-
+let array = strings.sorted(by: >)
 
 //: [Next](@next)
