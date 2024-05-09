5f0e7ff94f388dea9da0df6e2501cd951c505c53
Egor Egorov <egigoka@gmail.com>
2024-05-10 01:27:30 +0500

    course 1 lesson 4 theory

--- course1lesson4/Lesson 4.playground/Pages/while циклы.xcplaygroundpage/Contents.swift
+++ course1lesson4/Lesson 4.playground/Pages/while циклы.xcplaygroundpage/Contents.swift
@@ -12,7 +12,12 @@ import Foundation
  
  */
 
+var counter = 5
 
+while counter > 0 {
+    counter -= 1
+    print(counter)
+}
 
 /*:
  
@@ -22,6 +27,9 @@ import Foundation
  
  */
 
-
+repeat {
+    counter -= 1
+    print(counter)
+} while counter > 0
 
 //: [Next](@next)
