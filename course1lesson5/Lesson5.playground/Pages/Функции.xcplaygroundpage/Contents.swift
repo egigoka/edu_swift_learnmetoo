3ac1f12cb84f211b0ceb7680de18bd24a7d24ae6
Egor Egorov <egigoka@gmail.com>
2024-05-28 08:00:43 +0500

    course 1 lesson 5 theory

--- course1lesson5/Lesson5.playground/Pages/Функции.xcplaygroundpage/Contents.swift
+++ course1lesson5/Lesson5.playground/Pages/Функции.xcplaygroundpage/Contents.swift
@@ -35,10 +35,8 @@ addingTwoNumbers()
 func addingTwoNumbersWithReturn() -> Int {
     let a = 3
     let b = 2
-    let c = a + b
     
-    print(c)
-    return c
+    return a + b
 }
 
 var result = addingTwoNumbersWithReturn()
@@ -51,6 +49,31 @@ var result = addingTwoNumbersWithReturn()
     }
  */
 
+// Function with parameters and without arguments
+func addingTwoNumbers(a: Int, b: Int) -> Int {
+    a + b
+}
 
+result = addingTwoNumbers(a: 5, b: 7)
+
+// Function with parameters and arguments
+
+func addingTwoNumbers(number a: Int, andNumber b: Int) -> Int {
+    a + b
+}
+
+addingTwoNumbers(number: 7, andNumber: 3)
+
+func addingTwoNumbers(_ a: Int, _ b: Int) -> Int {
+    a + b
+}
+
+addingTwoNumbers(7, 3)
+
+func addingTwoNumbers(_ a: Int, and b: Int) -> Int {
+    a + b
+}
+
+addingTwoNumbers(9, and: 4)
 
 //: [Next](@next)
