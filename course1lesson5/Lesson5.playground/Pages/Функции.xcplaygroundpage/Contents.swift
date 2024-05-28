56290617f228bf082f6093e615351dad499318f0
Egor Egorov <egigoka@gmail.com>
2024-05-28 07:00:33 +0500

    course 1 lesson 5 theory

---
+++ course1lesson5/Lesson5.playground/Pages/Функции.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+
+import Foundation
+//: # Lesson 5
+
+//: ## Функции
+
+/*:
+ 
+    func nameOfFunction() {
+        some code
+    }
+ 
+ */
+
+
+
+//: ### Функции с возвращаемыми значениями
+
+/*:
+    func nameOfFunction() -> Data Type {
+        some code
+        return some value
+    }
+ 
+ */
+
+
+
+//: ### Функции с параметрами
+
+/*:
+    func name(argumentOne parameterOne: Data Type, argumentTwo parameterTwo: Data Type) {
+        some code
+    }
+ */
+
+
+
+//: [Next](@next)
