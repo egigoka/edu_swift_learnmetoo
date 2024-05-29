b162ce3a4a2e276fd3fc1e6556c23d701d4534fc
Egor Egorov <egigoka@gmail.com>
2024-05-30 01:02:36 +0500

    course 1 lesson 5 theory

--- course1lesson5/Lesson5.playground/Pages/Замыкания.xcplaygroundpage/Contents.swift
+++ course1lesson5/Lesson5.playground/Pages/Замыкания.xcplaygroundpage/Contents.swift
@@ -24,23 +24,49 @@ filterLessThanValue(value: someValue, numbers: numbers)
 
 //: Отбираем числа больше заданного значения
 
+func filterGreaterThanValue(value: Int, numbers: [Int]) -> [Int] {
+    var fileteredSetOfNumbers: [Int] = []
+    
+    for number in numbers {
+        if number > value {
+            fileteredSetOfNumbers.append(number)
+        }
+    }
+    return fileteredSetOfNumbers
+}
 
+filterGreaterThanValue(value: someValue, numbers: numbers)
 
 //: Функция для отбора чисел, относительно заданного значения
 
-
+func filterWithPredicateClosure(value: Int, numbers: [Int],
+                                closure: (Int, Int) -> Bool) -> [Int] {
+    var filteredNumbers: [Int] = []
+    
+    for number in numbers {
+        if closure(number, value) {
+            filteredNumbers.append(number)
+        }
+    }
+    return filteredNumbers
+}
 
 //: Функция для отбора числе меньше указанного значения
 
-
+func lessThanValue(number: Int, value: Int) -> Bool {
+    number < value
+}
 
 //: Функция для отбора числе больше указанного значения
 
-
+func greaterThanValue(number: Int, value: Int) -> Bool {
+    number > value
+}
 
 //: Отбор чисел меньше указанного значения
 
-
+filterWithPredicateClosure(value: someValue, numbers: numbers,
+                           closure: lessThanValue)
 
 //: Отбор чисел больше указанного значения
 
