068311434e75fb81eaf18cfa9c7f5331b5923d04
Egor Egorov <egigoka@gmail.com>
2024-08-26 22:34:58 +0500

    course 2 lesson 1 theory

---
+++ course2lesson1/HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by egigoka on 26/8/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
