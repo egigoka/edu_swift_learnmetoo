9c2a8388634e6b01e747923163b403ea0e2c37b7
Egor Egorov <egigoka@gmail.com>
2024-10-15 22:35:42 +0500

    course 2 lesson 2 theory

--- course2/lesson2/UIKitApp/UIKitApp/ViewController.swift
+++ course2/lesson2/UIKitApp/UIKitApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Label
+        mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        // Segmented control
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        
     }
 
-
+    @IBAction func segmentedControlAction() {
+    }
+    
+    
 }
 
