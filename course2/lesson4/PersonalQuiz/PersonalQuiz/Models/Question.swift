d6226744906f0a12b86ff33cb4cf4025d5ecf591
Egor Egorov <egigoka@gmail.com>
2024-10-24 08:04:22 +0500

    course2 lesson 4 theory

--- course2/lesson4/PersonalQuiz/PersonalQuiz/Models/Question.swift
+++ course2/lesson4/PersonalQuiz/PersonalQuiz/Models/Question.swift
@@ -16,3 +16,40 @@ struct Question {
     let type: ResponseType
     let answers: [Answer]
 }
+
+extension Question {
+    static func getQuestions() -> [Question] {
+        return [
+            Question(
+                text: "Какую еду Вы предпочитаете?",
+                type: .single,
+                answers: [
+                    Answer(text: "Стейк", type: .dog),
+                    Answer(text: "Рыба", type: .cat),
+                    Answer(text: "Морковь", type: .rabbit),
+                    Answer(text: "Кукуруза", type: .turtle)
+                ]
+            ),
+            Question(
+                text: "Что Вам нравится больше?",
+                type: .multiple,
+                answers: [
+                    Answer(text: "Плавать", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Обниматься", type: .rabbit),
+                    Answer(text: "Есть", type: .turtle)
+                ]
+            ),
+            Question(
+                text: "Любите ли Вы поездки на машине?",
+                type: .ranged,
+                answers: [
+                    Answer(text: "Ненавижу", type: .cat),
+                    Answer(text: "Нервничаю", type: .rabbit),
+                    Answer(text: "Не замечаю", type: .turtle),
+                    Answer(text: "Обожаю", type: .dog)
+                ]
+            )
+        ]
+    }
+}
