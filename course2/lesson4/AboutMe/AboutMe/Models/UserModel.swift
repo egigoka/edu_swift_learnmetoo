d4dddf37d1cb2b86274cf8b671ee72d7c12c7cd8
Egor Egorov <egigoka@gmail.com>
2024-10-27 21:25:53 +0500

    course2 lesson 4 homework

--- course2/lesson4/AboutMe/AboutMe/Models/UserModel.swift
+++ course2/lesson4/AboutMe/AboutMe/Models/UserModel.swift
@@ -5,14 +5,12 @@
 //  Created by egigoka on 27/10/2024.
 //
 
-import UIKit
-
 struct User {
     let login: String
     let password: String
     
     let username: String
-    let image: UIImage?
+    let imageName: String
 }
 
 extension User {
@@ -21,11 +19,11 @@ extension User {
             User(login: "root",
                  password: "toor",
                  username: "EGiGoka",
-                 image: UIImage(named: "RootImage")),
+                 imageName: "RootImage"),
             User(login: "guest",
                  password: "guestlol",
                  username: "Guest",
-                 image: UIImage(named: "GuestImage"))
+                 imageName: "GuestImage")
         ]
     }
 }
