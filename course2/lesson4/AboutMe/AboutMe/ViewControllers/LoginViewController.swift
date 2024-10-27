27bd29d0fbaef918c888cfd9fdfc5e420ac4fb5d
Egor Egorov <egigoka@gmail.com>
2024-10-27 20:04:57 +0500

    course2 lesson 4 homework

--- course2/lesson4/AboutMe/AboutMe/ViewControllers/LoginViewController.swift
+++ course2/lesson4/AboutMe/AboutMe/ViewControllers/LoginViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: IB Outlets
     @IBOutlet var loginTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
+    // MARK: Private properties
+    private let users = User.getUsers()
+    
+    // MARK: Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // MARK: IB Actions
     @IBAction func loginButtonAction() {
         
     }
     
     @IBAction func forgotLoginButtonAction(_ sender: Any) {
+        var usersString = users.compactMap {$0.login} .joined(separator: ", ")
+        showAlert(with: "No problem", and: "Available users: \(usersString)")
     }
     
     @IBAction func forgotPasswordButtonAction(_ sender: Any) {
     }
     
+    // MARK: Private methods
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            // nothing as for now
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
