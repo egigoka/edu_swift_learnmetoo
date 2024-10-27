450a9d18af9d30de2e6244d8dafe1df73c18607c
Egor Egorov <egigoka@gmail.com>
2024-10-27 19:44:50 +0500

    course2 lesson 4 homework

--- course2/lesson4/AboutMe/AboutMe/ViewControllers/LoginViewController.swift
+++ course2/lesson4/AboutMe/AboutMe/ViewControllers/LoginViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func loginButtonAction() {
+        
+    }
+    
+    @IBAction func forgotLoginButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func forgotPasswordButtonAction(_ sender: Any) {
+    }
     
 }
