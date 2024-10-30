a422eee5ca921f03c35412f2b95d605f05497857
Egor Egorov <egigoka@gmail.com>
2024-10-30 22:28:48 +0500

    course2 lesson 5 homework review

---
+++ course2/lesson5/PersonalQuiz/PersonalQuiz/View Controllers/QuestionsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  QuestionsViewController.swift
+//  PersonalQuiz
+//
+//  Created by egigoka on 30.10.2024.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+    
+    @IBOutlet var questionNumberLabel: UILabel!
+    
+}
