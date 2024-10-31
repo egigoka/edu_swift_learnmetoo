bca3f32d4c49fb7dee7ae5d0a634ff7a9defb2be
Egor Egorov <egigoka@gmail.com>
2024-10-31 23:50:24 +0500

    course2 lesson 5 theory

--- course2/lesson5/PersonalQuiz/PersonalQuiz/View Controllers/ResultsViewController.swift
+++ course2/lesson5/PersonalQuiz/PersonalQuiz/View Controllers/ResultsViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
-
+// MARK: - Class/Struct Declaration
+// MARK: - IB Outlets
+// MARK: - Public Properties
+// MARK: - Private Properties
+// MARK: - Initializers
+// MARK: - Lifecycle Methods
+// MARK: - Deinitializer
+// MARK: - Override Methods
+// MARK: - Navigation
+// MARK: - IB Actions
+// MARK: - Public Methods
+// MARK: - Private Methods
+// MARK: - Error Handling
+// MARK: - Protocol Conformance
+// MARK: - Static Methods
+// MARK: - Closures and Callbacks
 class ResultsViewController: UIViewController {
-
-    // MARK: - Public Properties
-    var chosenAnswers: [Answer] = []
     
+    // MARK: - IB Outlets
+    @IBOutlet var animalEmojiLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    
+    // MARK: - Public Properties
+    var answersChosen: [Answer]!
+    
+    // MARK: - Private Properties
+    private var result: AnimalType?
+    
+    // MARK: - Ovverride Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(answersChosen.count)
+        countAnswers()
+        
     }
-
+    
+    // MARK: - Private Methods
+    private func countAnswers() {
+        var results: [String: Int] = [:]
+        for answer in answersChosen {
+            let animal = "\(answer.type)"
+            if results.keys.contains(animal) {
+                results.updateValue((results[animal] ?? 0) + 1, forKey: animal)
+            } else {
+                results[animal] = 1
+            }
+        }
+    }
+    
+    private func updateUI() {
+        
+    }
 }
