042a045e81acd99b1c0d5924c7c9c7d10a9aaaca
Egor Egorov <egigoka@gmail.com>
2024-11-28 23:57:50 +0500

    course2 lesson 6 homework review

---
+++ course2/lesson6/Lesson 6.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+//: ## Протоколы
+
+
+
+//: ### CustomStringConvertable
+
+let string = "Hello, world!"
+print(string)
+
+let number = 10
+print(number)
+
+let bool = true
+print(bool)
+
+//: ### Equatable
+
+
+
+//: ### Comparable
+ 
+
+
+//: ### Создание собственного протокола
+
+
+
+//: ### Делегирование
+
+
