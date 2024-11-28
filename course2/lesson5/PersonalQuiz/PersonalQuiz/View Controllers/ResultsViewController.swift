979a65a930faac03bf274b974b5d8b1bf93c15ba
Egor Egorov <egigoka@gmail.com>
2024-11-28 22:33:28 +0500

    course2 lesson 6 homework review

--- course2/lesson5/PersonalQuiz/PersonalQuiz/View Controllers/ResultsViewController.swift
+++ course2/lesson5/PersonalQuiz/PersonalQuiz/View Controllers/ResultsViewController.swift
@@ -27,23 +27,34 @@ class ResultsViewController: UIViewController {
     
     // MARK: - Private Methods
     private func countAnswers() {
-        var results: [AnimalType: Int] = [:]
-        let animals = answersChosen.map { $0.type }
-        for animal in animals {
-            if let animalCount = results[animal] {
-                results.updateValue(animalCount + 1, forKey: animal)
-            } else {
-                results[animal] = 1
-            }
-        }
-        let sortedResults = results.sorted { $0.value > $1.value }
-        guard let mostFrequent = sortedResults.first?.key else { return }
+//        var results: [AnimalType: Int] = [:]
+//        let animals = answersChosen.map { $0.type }
+        
+//        for animal in animals {
+//            if let animalCount = results[animal] {
+//                results.updateValue(animalCount + 1, forKey: animal)
+//            } else {
+//                results[animal] = 1
+//            }
+//        }
+        
+//        for animal in animals {
+//            results[animal] = (results[animal] ?? 0) + 1
+//        }
+//        
+//        let sortedResults = results.sorted { $0.value > $1.value }
+//        guard let mostFrequent = sortedResults.first?.key else { return }
+        
+        // one liner, but not sure if it's good
+        let mostFrequent = Dictionary(grouping: answersChosen, by: { $0.type })
+            .sorted(by: { $0.value.count > $1.value.count })
+            .first?.key
         
         updateUI(with: mostFrequent)
     }
     
-    private func updateUI(with animal: AnimalType) {
-        animalEmojiLabel.text = "Вы - \(animal.rawValue)"
-        descriptionLabel.text = animal.definition
+    private func updateUI(with animal: AnimalType?) {
+        animalEmojiLabel.text = "Вы - \(animal?.rawValue ?? "🐶")"
+        descriptionLabel.text = animal?.definition ?? ""
     }
 }
