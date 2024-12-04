923cdf2adace9dd6b503651bda9281c649094e15
Egor Egorov <egigoka@gmail.com>
2024-12-04 06:47:34 +0500

    course2 lesson 6 theory

--- course2/lesson6/Lesson 6.playground/Contents.swift
+++ course2/lesson6/Lesson 6.playground/Contents.swift
@@ -15,9 +15,54 @@ print(number)
 let bool = true
 print(bool)
 
+class Shoes: CustomStringConvertible {
+    
+    let color: String
+    let size: Int
+    let hasLaces: Bool
+    
+    var description: String {
+        "Shoes(color: \"\(color)\", size: \(size), hasLaces: \(hasLaces))"
+    }
+    
+    init(color: String, size: Int, hasLaces: Bool) {
+        self.color = color
+        self.size = size
+        self.hasLaces = hasLaces
+    }
+    
+    
+}
+
+let myShoes = Shoes(color: "Brown", size: 41, hasLaces: true)
+print(myShoes)
+
 //: ### Equatable
 
+struct Employee {
+    let firstName: String
+    let lastName: String
+    let jobTitle: String
+    let phoneNumber: String
+}
 
+let employees = [
+    Employee(
+        firstName: "Alexey",
+        lastName: "Efimov",
+        jobTitle: "Teacher",
+        phoneNumber: "+7-777-777-77-77"),
+    Employee(
+        firstName: "Ivan",
+        lastName: "Akulov",
+        jobTitle: "Head",
+        phoneNumber: "+7-777-888-88-88"),
+    Employee(
+        firstName: "Ivan",
+        lastName: "Danilin",
+        jobTitle: "Teacher",
+        phoneNumber: "+7-777-999-99-99")
+]
 
 //: ### Comparable
  
