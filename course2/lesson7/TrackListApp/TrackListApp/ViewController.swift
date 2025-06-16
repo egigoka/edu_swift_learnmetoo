dae99cc784800d09ebbcefeed99bfbfaf0d725c1
Egor Egorov <egigoka@gmail.com>
2025-06-16 23:56:17 +0500

    course 2 lesson 7 theory

---
+++ course2/lesson7/TrackListApp/TrackListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackListApp
+//
+//  Created by egigoka on 16.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
