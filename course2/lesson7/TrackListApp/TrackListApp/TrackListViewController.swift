7d2b1cd5d5cd5ddf464cd05927f9389f5f809a15
Egor Egorov <egigoka@gmail.com>
2025-06-18 22:29:31 +0500

    course 2 lesson 7 theory

---
+++ course2/lesson7/TrackListApp/TrackListApp/TrackListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TrackListViewController.swift
+//  TrackListApp
+//
+//  Created by egigoka on 18.06.2025.
+//
+
+import UIKit
+
+class TrackListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "trackName", for: indexPath)
+
+        
+        
+        return cell
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+}
