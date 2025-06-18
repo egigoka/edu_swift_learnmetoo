64f5fed99d18deb996622d3e0dfee15f94e14434
Egor Egorov <egigoka@gmail.com>
2025-06-18 23:12:04 +0500

    course 2 lesson 7 theory

--- course2/lesson7/TrackListApp/TrackListApp/Track.swift
+++ course2/lesson7/TrackListApp/TrackListApp/Track.swift
@@ -18,9 +18,16 @@ extension Track {
     static func getTrackList() -> [Track] {
         [
             Track(artist: "Alberto Ruiz", song: "7 Elements (Original Mix)"),
-            Track(artist: "Deve Wincent", song: "Red Eye (Original Mix)"),
-            
-            
+            Track(artist: "Dave Wincent", song: "Red Eye (Original Mix)"),
+            Track(artist: "E-Spectro", song: "End Station (Original Mix)"),
+            Track(artist: "Edna Ann", song: "Phasma (Konstantin Yoodza Remix)"),
+            Track(artist: "Ilija Djokovic", song: "Delusion (Original Mix)"),
+            Track(artist: "John Baptiste", song: "fingerprint (Original Mix)"),
+            Track(artist: "Lane 8", song: "Fingerprint (Original Mix)"),
+            Track(artist: "Mac Vaughn", song: "Pink Is My Favorite Color (Alex Stein Remix)"),
+            Track(artist: "Metodi Hristov, Gallya", song: "Badmash (Original Mix)"),
+            Track(artist: "Veerus, Maxie Devine", song: "Nightmare (Original Mix)")
+            Track(artist: "Infected Mushroom", song: "Becoming Insane")
         ]
     }
 }
