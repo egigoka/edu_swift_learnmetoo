1118af7e7522557220fbbf76e9ccd9ebb26d4041
Egor Egorov <egigoka@gmail.com>
2025-06-18 23:12:04 +0500

    course 2 lesson 7 theory

--- course2/lesson7/TrackListApp/TrackListApp/TrackListViewController.swift
+++ course2/lesson7/TrackListApp/TrackListApp/TrackListViewController.swift
@@ -9,27 +9,28 @@ import UIKit
 
 class TrackListViewController: UITableViewController {
 
+    var trackList = Track.getTrackList()
+    
+    // MARK: - Main View Controller methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
     // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        trackList.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "trackName", for: indexPath)
 
-        
+        cell.
         
         return cell
     }
 
     // MARK: - Navigation
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     }
 
