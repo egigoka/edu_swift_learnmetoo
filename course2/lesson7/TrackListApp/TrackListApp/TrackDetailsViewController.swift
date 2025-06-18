4f3f8c4de7da576db41da046c542a166a8b94f95
Egor Egorov <egigoka@gmail.com>
2025-06-18 23:57:21 +0500

    course 2 lesson 7 theory

---
+++ course2/lesson7/TrackListApp/TrackListApp/TrackDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrackDetailsViewController.swift
+//  TrackListApp
+//
+//  Created by egigoka on 18.06.2025.
+//
+
+import UIKit
+
+class TrackDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
