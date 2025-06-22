21677b82bf7c682026ae45c0b45127a4bc916ecb
Egor Egorov <egigoka@gmail.com>
2025-06-23 00:04:36 +0500

    course 2 lesson 7 homework

---
+++ course2/lesson7/RandomContacts/RandomContacts/TabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarController.swift
+//  RandomContacts
+//
+//  Created by egigoka on 22.06.2025.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    let persons = Person.getRandomItems()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
