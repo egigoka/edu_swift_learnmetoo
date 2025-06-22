3ff0b990294b61511bec42efc83537f858dc53f5
Egor Egorov <egigoka@gmail.com>
2025-06-22 22:05:09 +0500

    course 2 lesson 7 theory

--- course2/lesson7/TrackListApp/TrackListApp/TrackDetailsViewController.swift
+++ course2/lesson7/TrackListApp/TrackListApp/TrackDetailsViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class TrackDetailsViewController: UIViewController {
 
+    @IBOutlet var artCoverImageView: UIImageView!
+    @IBOutlet var trackLabel: UILabel!
+    
+    var track: Track!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        artCoverImageView.image = UIImage(named: track.track)
+        trackLabel.text = track.track
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
