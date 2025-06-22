b1a333e7290eaa79bec12be80818e5139948c2e1
Egor Egorov <egigoka@gmail.com>
2025-06-23 00:34:57 +0500

    course 2 lesson 7 homework

--- course2/lesson7/RandomContacts/RandomContacts/ContactsTableViewController.swift
+++ course2/lesson7/RandomContacts/RandomContacts/ContactsTableViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ContactsTableViewController: UITableViewController {
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     let people: [Person]!
     
     override func viewDidLoad() {
@@ -36,7 +40,7 @@ class ContactsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
         let person = people[indexPath.row]
-        cell.text
+        cell.textLabel?.text = "\(person.name) \(person.surname)"
         return cell
     }
     
