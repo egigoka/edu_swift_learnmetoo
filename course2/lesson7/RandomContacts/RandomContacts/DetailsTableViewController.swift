82c377c0466a3cc9825adfc34e4fe5eebb69d01b
Egor Egorov <egigoka@gmail.com>
2025-06-29 20:23:36 +0500

    course 2 lesson 7 homework

--- course2/lesson7/RandomContacts/RandomContacts/DetailsTableViewController.swift
+++ course2/lesson7/RandomContacts/RandomContacts/DetailsTableViewController.swift
@@ -13,18 +13,15 @@ class DetailsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let destination = segue.destination as? PersonViewController else { return }
+        guard let selectedIndexPath = tableView.indexPathForSelectedRow else { return }
+        destination.person = people[selectedIndexPath.section]
     }
-    */
     
 }
 
@@ -33,7 +30,6 @@ extension DetailsTableViewController : PeopleReceiving { }
 
 // MARK: - Table view data source
 extension DetailsTableViewController {
-    
     override func numberOfSections(in tableView: UITableView) -> Int {
         people.count
     }
@@ -45,43 +41,31 @@ extension DetailsTableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         "\(people[section].name) \(people[section].surname)"
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let personIndex = indexPath.row / 3
-//        let cellType = indexPath.row % 3
         let personIndex = indexPath.section
         let cellType = indexPath.row % 2
         let cell = tableView.dequeueReusableCell(withIdentifier: "contactDetail", for: indexPath)
         let person = people[personIndex]
         
         var detailText = ""
+        var image = UIImage(systemName: "person.fill.questionmark")
         switch cellType {
         case 0:
-            //detailText = "\(person.name) \(person.surname)"
-            detailText = person.email
-        case 1:
             detailText = person.phoneNumber
-//        case 2:
-//            detailText = person.phoneNumber
+            image = UIImage(systemName: "phone")?.withTintColor(.green, renderingMode: .alwaysOriginal)
+        case 1:
+            detailText = person.email
+            image = UIImage(systemName: "tray")?.withTintColor(.blue, renderingMode: .alwaysOriginal)
         default:
             break
         }
         
         cell.textLabel?.text = detailText
+        cell.imageView?.image = image
         
         return cell
     }
-    
-    // MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let destination = segue.destination as? PersonViewController else { return }
-        guard let selectedIndexPath = tableView.indexPathForSelectedRow else { return }
-        destination.person = people[selectedIndexPath.row]
-    }
 }
-
-
-// MARK: - Table view data source
 
 
