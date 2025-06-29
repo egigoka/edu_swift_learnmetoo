98436d2ccc9d44947bc1564eccd74da3e330fca5
Egor Egorov <egigoka@gmail.com>
2025-06-29 23:34:03 +0500

    course 2 lesson 8 theory

--- apps/LifeCycleViewController/LifeCycleViewController/GreenViewController.swift
+++ apps/LifeCycleViewController/LifeCycleViewController/GreenViewController.swift
@@ -10,93 +10,27 @@ import UIKit
 
 class GreenViewController: UIViewController {
     
-    @IBOutlet var openThirdVC: UIButton! {
-        didSet {
-            print("\(openThirdVC.currentTitle ?? "")")
-        }
-    }
+    @IBOutlet var openThirdVC: UIButton!
     
-    var someProperties: String! {
-        didSet {
-            print(someProperties ?? "")
-        }
-    }
-    
-    override class func awakeFromNib() {
-        super.awakeFromNib()
-//        printMessage()
-    }
-    
-//    override func loadView() {
-//        
-//    }
+    var someProperties: String!
     
     // Срабатывает после загрузки View
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "GreenVC"
-        printMessage()
+        //title = "GreenVC"
+        //printMessage()
     }
     
-    
-    // Срабатывает перед появленем вью на экране
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        printMessage()
-    }
-    
+    // Срабатывает перед появлением вью на экране
     
     // Вызывается при расстановке констрейнтов
-    override func updateViewConstraints() {
-        printMessage()
-        super.updateViewConstraints()
-    }
-    
     
     // Срабатывает перед тем, как размер вью поменяется под размер экрана
-    override func viewWillLayoutSubviews() {
-        printMessage()
-    }
-
     
     // Тут срабатывает Auto Layout
     
-    
-    // Срабатывает после того, как размер вью изменился под размер экрана
-    override func viewDidLayoutSubviews() {
-        printMessage()
-    }
-
+    // Срабатывает после того, как размер вью изменился под размер эрана
     
     // Срабатывает после появления вью на экране
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        printMessage()
-    }
-
     
-    // Срабатывает при повороте экрана
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        printMessage()
-    }
-    
-    
-    // Срабатывает перед тем, как вью закроется
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        printMessage()
-    }
-
-    
-    // Срабатывает после закрытия вью
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        printMessage()
-    }
-}
-
-extension UIViewController {
-    func printMessage(function: String = #function) {
-        print("\(title ?? ""): \(function)")
-    }
 }
