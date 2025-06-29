034ec043ad2f8f72f241bbada6cb444128bde57c
Egor Egorov <egigoka@gmail.com>
2025-06-29 21:55:44 +0500

    course 2 lesson 8 homework review

--- apps/RandomContacts/RandomContacts/TabBarController.swift
+++ apps/RandomContacts/RandomContacts/TabBarController.swift
@@ -19,26 +19,6 @@ class TabBarController: UITabBarController {
                 tableVC.people = people
             }
         }
-        
-        for navigationController in viewControllers ?? [] {
-            if let navigationController = navigationController as? UINavigationController {
-                if var tableVC = navigationController.viewControllers[0] as? PeopleReceiving {
-                    tableVC.people = people
-                }
-            }
-        }
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
