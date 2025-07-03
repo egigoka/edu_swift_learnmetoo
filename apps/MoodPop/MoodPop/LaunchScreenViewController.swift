466a174f11a343d5902e26b7f4a331728ffb2ecf
Egor Egorov <egigoka@gmail.com>
2025-07-03 05:12:49 +0500

    course 2 lesson 8 homework

--- apps/MoodPop/MoodPop/LaunchScreenViewController.swift
+++ apps/MoodPop/MoodPop/LaunchScreenViewController.swift
@@ -9,21 +9,14 @@ import UIKit
 
 class LaunchScreenViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet var bubbleView: UIView!
+    
+    override func viewDidLayoutSubviews() {
+        roundBubbleView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func roundBubbleView() {
+        bubbleView.layer.cornerRadius = bubbleView.frame.height / 2
     }
-    */
 
 }
