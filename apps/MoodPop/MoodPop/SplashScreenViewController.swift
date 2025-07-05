c6264d54a22c2286c7213c7becef8b9020ad2925
Egor Egorov <egigoka@gmail.com>
2025-07-05 20:03:58 +0500

    course 2 lesson 8 homework

--- apps/MoodPop/MoodPop/SplashScreenViewController.swift
+++ apps/MoodPop/MoodPop/SplashScreenViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 class SplashScreenViewController: UIViewController {
 
     @IBOutlet var bubbleView: UIView!
-    @IBOutlet var ratioConstraint: NSLayoutConstraint!
+    @IBOutlet var ratioBubbleConstraint: NSLayoutConstraint!
     @IBOutlet var tapView: UIView!
+    @IBOutlet var ratioTapConstraint: NSLayoutConstraint!
     
     var debugBubbleTapCounter = 0
     var bubbleEnlarged = false
@@ -47,18 +48,22 @@ class SplashScreenViewController: UIViewController {
     }
     
     private func roundBubble() {
-        //bubbleView.layer.cornerRadius = bubbleView.frame.height / 2
+        bubbleView.layer.cornerRadius = bubbleView.frame.height / 2
+        tapView.layer.cornerRadius = tapView.frame.height / 2
     }
     
     private func modifyBubble(to multiplier: CGFloat){
-        guard let constraintView = ratioConstraint.firstItem as? UIView else {
+        guard let ratioBubbleConstraintFirstItem = ratioBubbleConstraint.firstItem as? UIView else {
+            return
+        }
+        guard let ratioTapConstraintFirstItem = ratioTapConstraint.firstItem as? UIView else {
             return
         }
         
-        let newConstraintRatio = ratioConstraint.constraintWithMultiplier(multiplier)
-        constraintView.removeConstraint(ratioConstraint)
-        view.addConstraint(newConstraintRatio)
-        UIView.animate(withDuration: 1.0,
+        let newRatioBubbleConstraint = ratioBubbleConstraint.constraintWithMultiplier(multiplier)
+        ratioBubbleConstraintFirstItem.removeConstraint(ratioBubbleConstraint)
+        view.addConstraint(newRatioBubbleConstraint)
+        UIView.animate(withDuration: 3,
                            delay: 0,
                            usingSpringWithDamping: 0.7,
                            initialSpringVelocity: 0.5,
@@ -67,7 +72,12 @@ class SplashScreenViewController: UIViewController {
             
         }
         
-        ratioConstraint = newConstraintRatio
+        let newRatioTapConstraint = ratioTapConstraint.constraintWithMultiplier(multiplier)
+        ratioTapConstraintFirstItem.removeConstraint(ratioTapConstraint)
+        view.addConstraint(newRatioTapConstraint)
+        
+        ratioBubbleConstraint = newRatioBubbleConstraint
+        ratioTapConstraint = newRatioTapConstraint
         
         // debug
 //        for constraint in bubbleView.constraints {
