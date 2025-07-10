c28581d3ef3747105dae19432917dd1ff382da7a
Egor Egorov <egigoka@gmail.com>
2025-07-11 04:55:28 +0500

    course 2 lesson 9 theory

--- apps/AnimationApp/AnimationApp/ViewController.swift
+++ apps/AnimationApp/AnimationApp/ViewController.swift
@@ -11,12 +11,28 @@ class ViewController: UIViewController {
 
     @IBOutlet var coreAnimationView: UIView!
     
+    private var originCoordinate: CGFloat?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        originCoordinate = coreAnimationView.frame.origin.x
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        originCoordinate = coreAnimationView.frame.origin.x
     }
 
     @IBAction func coreAnimationButtonPressed(_ sender: UIButton) {
+        sender.pulsate()
         
+        if coreAnimationView.frame.origin.x == originCoordinate {
+            coreAnimationView.frame.origin.x -= 40
+            print("moved")
+        }
+        
+        print(originCoordinate)
+        print(coreAnimationView.frame.origin.x)
     }
     
 }
