5fbf6c836a862b8cb78e84ccb1bd6c390f491365
Egor Egorov <egigoka@gmail.com>
2025-07-12 11:31:40 +0500

    course 2 lesson 9 homework

---
+++ apps/AnimationAppHW/AnimationAppHW/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AnimationAppHW
+//
+//  Created by egigoka on 12.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var animationView: SpringView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
+
