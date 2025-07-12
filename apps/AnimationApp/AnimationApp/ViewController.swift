ee5d4ba26388aba93bbb314aaa26bba19945f514
Egor Egorov <egigoka@gmail.com>
2025-07-12 11:01:34 +0500

    course 2 lesson 9 theory

--- apps/AnimationApp/AnimationApp/ViewController.swift
+++ apps/AnimationApp/AnimationApp/ViewController.swift
@@ -17,28 +17,32 @@ class ViewController: UIViewController {
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        originCoordinate = coreAnimationView.frame.origin.x
+        if originCoordinate == nil ||  originCoordinate != 0 {
+            originCoordinate = coreAnimationView.frame.origin.x
+            print("updated")
+        }
     }
 
     @IBAction func coreAnimationButtonPressed(_ sender: UIButton) {
         sender.pulsate()
-        guard originCoordinate == coreAnimationView.frame.origin.x else { return }
         UIView.animate(withDuration: 0.5,
                        delay: 0,
                        options: [.autoreverse, .repeat]) {
             if self.coreAnimationView.frame.origin.x == self.originCoordinate {
                 self.coreAnimationView.frame.origin.x -= 40
-                self.originCoordinate = 0
             }
         }
-        
-        print(originCoordinate ?? 0)
-        print(coreAnimationView.frame.origin.x)
+        originCoordinate = 0
     }
     
     @IBAction func springAnimationButtonPressed(_ sender: SpringButton) {
+        springAnimationView.animation = "wobble"
+        springAnimationView.curve = "easeIn"
+        springAnimationView.duration = 1
+        springAnimationView.animate()
         
-        
+        sender.animation = "shake"
+        sender.animate()
     }
     
     
