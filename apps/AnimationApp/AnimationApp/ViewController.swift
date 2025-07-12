0f21fad2f3cf310ba7db8ed4e520fa288d7ddce7
Egor Egorov <egigoka@gmail.com>
2025-07-12 10:48:10 +0500

    course 2 lesson 9 theory

--- apps/AnimationApp/AnimationApp/ViewController.swift
+++ apps/AnimationApp/AnimationApp/ViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
 
     @IBOutlet var coreAnimationView: UIView!
+    @IBOutlet var springAnimationView: SpringView!
     
     private var originCoordinate: CGFloat?
     
@@ -20,18 +22,24 @@ class ViewController: UIViewController {
 
     @IBAction func coreAnimationButtonPressed(_ sender: UIButton) {
         sender.pulsate()
-        
+        guard originCoordinate == coreAnimationView.frame.origin.x else { return }
         UIView.animate(withDuration: 0.5,
                        delay: 0,
                        options: [.autoreverse, .repeat]) {
-            print(self.coreAnimationView.frame.origin.x)
             if self.coreAnimationView.frame.origin.x == self.originCoordinate {
                 self.coreAnimationView.frame.origin.x -= 40
+                self.originCoordinate = 0
             }
         }
         
         print(originCoordinate ?? 0)
         print(coreAnimationView.frame.origin.x)
     }
+    
+    @IBAction func springAnimationButtonPressed(_ sender: SpringButton) {
+        
+        
+    }
+    
     
 }
