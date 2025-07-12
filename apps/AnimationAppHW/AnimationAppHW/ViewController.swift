1eca886db7c867f882a9122e8a7646436c1982b9
Egor Egorov <egigoka@gmail.com>
2025-07-12 12:17:18 +0500

    course 2 lesson 10 homework review

--- apps/AnimationAppHW/AnimationAppHW/ViewController.swift
+++ apps/AnimationAppHW/AnimationAppHW/ViewController.swift
@@ -24,20 +24,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonPressed() {
-        animationView.animation = nextAnimation?.preset ?? "wobble"
-        animationView.curve = nextAnimation?.curve ?? "easeIn"
-        animationView.duration = nextAnimation?.duration ?? 1
-        animationView.force = nextAnimation?.force ?? 1
-        animationView.
+        applyNextAnimation()
+        updateLabel()
+        updateNextAnimation()
+        updateButtonText()
+        animationView.animate()
     }
     
     private func updateNextAnimation() {
         let preset = AnimationPreset.allCases.randomElement()?.rawValue ?? ""
         let curve = AnimationCurve.allCases.randomElement()?.rawValue ?? ""
-        let duration = Double.random(in: 0...2)
-        let force = Double.random(in: 0...2)
-        let damping = Double.random(in: 0...1)
-        let velocity = Double.random(in: 0...2)
+        let duration = Double.random(in: 0.3...2)
+        let force = Double.random(in: 0.1...2)
+        let damping = Double.random(in: 0.1...1)
+        let velocity = Double.random(in: 0.1...2)
         nextAnimation = Animation(preset: preset, curve: curve, duration: duration, force: force, damping: damping, velocity: velocity)
     }
     
@@ -45,10 +45,19 @@ class ViewController: UIViewController {
         animationLabel.text = """
         Animation: \(animationView.animation)
         Curve: \(animationView.curve)
-        Duration: \(animationView.duration)
-        Force: \(animationView.force)
-        Damping: \(animationView.damping)
+        Duration: \(String(format: "%.2f", animationView.duration))
+        Force: \(String(format: "%.2f", animationView.force))
+        Damping: \(String(format: "%.2f", animationView.damping))
         """
+    }
+    
+    private func applyNextAnimation() {
+        animationView.animation = nextAnimation?.preset ?? "wobble"
+        animationView.curve = nextAnimation?.curve ?? "easeIn"
+        animationView.duration = nextAnimation?.duration ?? 1
+        animationView.force = nextAnimation?.force ?? 1
+        animationView.damping = nextAnimation?.damping ?? 1
+        animationView.velocity = nextAnimation?.velocity ?? 1
     }
     
     private func updateButtonText() {
