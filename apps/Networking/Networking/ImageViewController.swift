f7ea8f7414726f055e7ac220e15b6381f8ba027a
Egor Egorov <egigoka@gmail.com>
2025-07-27 17:58:44 +0500

    course 2 lesson 10 theory

--- apps/Networking/Networking/ImageViewController.swift
+++ apps/Networking/Networking/ImageViewController.swift
@@ -14,7 +14,32 @@ class ImageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        fetchImage()
+    }
+    
+    private func fetchImage() {
+        guard let url = URL(string: URLExamples.imageURL.rawValue) else { return }
         
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+            
+        }.resume()
     }
 }
 
