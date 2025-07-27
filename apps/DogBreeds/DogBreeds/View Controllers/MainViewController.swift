c0b1e93758d4ab09fb24c237a2368753c5d994a4
Egor Egorov <egigoka@gmail.com>
2025-07-27 22:50:20 +0500

    course 2 lesson 10 homework

--- apps/DogBreeds/DogBreeds/View Controllers/MainViewController.swift
+++ apps/DogBreeds/DogBreeds/View Controllers/MainViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class RootViewController: UITableViewController {
 
+    private var breeds: [Breed] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
@@ -22,11 +24,41 @@ class RootViewController: UITableViewController {
         ) { result in
             switch result {
             case .success(let response):
-                print(response)
+                self.populateBreeds(from: response.message)
             case .failure(let error):
                 print(error)
             }
         }
+        for breed in breeds {
+            var url: URL?
+            switch breed.type {
+            case .breed:
+                url = URL(string: Url.randomImage(breed: breed.name).urlString)
+            case .subBreed:
+                guard let subBreed = breed.subBreed else {continue}
+                url = URL(string: Url.randomImageWithSubBreed(
+                    breed: breed.name,
+                    subBreed: subBreed).urlString)
+            
+            }
+        }
+        
     }
+    
+    private func populateBreeds(from breedsDict: Dictionary<String, [String]>) {
+        breeds = []
+        for (breed, subBreeds) in breedsDict {
+            print(breed, subBreeds)
+            if subBreeds.count == 0 {
+                self.breeds.append(Breed(name: breed, subBreed: nil, type: .breed))
+            } else {
+                for subBreed in subBreeds {
+                    self.breeds.append(Breed(name: breed, subBreed: subBreed, type: .subBreed))
+                }
+            }
+        }
+    }
+    
+    
 
 }
