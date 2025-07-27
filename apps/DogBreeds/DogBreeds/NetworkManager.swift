4cabeaefd334cbe88ab0dee6e3fb50de96bb9c6d
Egor Egorov <egigoka@gmail.com>
2025-07-27 21:20:06 +0500

    course 2 lesson 10 homework

---
+++ apps/DogBreeds/DogBreeds/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  DogBreeds
+//
+//  Created by egigoka on 27.07.2025.
+//
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+}
