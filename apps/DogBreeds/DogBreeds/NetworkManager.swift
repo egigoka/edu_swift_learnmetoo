17513bd8d0d1ffb2386208f5db612afb0ee90fda
Egor Egorov <egigoka@gmail.com>
2025-07-27 21:34:36 +0500

    course 2 lesson 10 homework

--- apps/DogBreeds/DogBreeds/NetworkManager.swift
+++ apps/DogBreeds/DogBreeds/NetworkManager.swift
@@ -5,8 +5,36 @@
 //  Created by egigoka on 27.07.2025.
 //
 
+import Foundation
+
 class NetworkManager {
     static let shared = NetworkManager()
+    private let session = URLSession.shared
     
     private init() {}
+    
+    func fetch<T: Decodable>(
+        from url: URL,
+        responseType: T.Type,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data"])))
+                return
+            }
+            
+            do {
+                let decoded = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decoded))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
 }
