3cb980eb4ea0a4febf7dc47a5d7051ac3284a426
Egor Egorov <egigoka@gmail.com>
2025-07-27 17:29:56 +0500

    course 2 lesson 10 theory

---
+++ apps/Networking/Networking/UserActionCell.swift
@@ -0,0 +1,12 @@
+//
+//  UserActionCell.swift
+//  Networking
+//
+//  Created by egigoka on 27.07.2025.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    @IBOutlet var userActionLabel: UILabel!
+}
