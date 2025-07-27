43687423f8c4bdb88fcdfddd83a693b0b97fcef6
Egor Egorov <egigoka@gmail.com>
2025-07-27 23:04:53 +0500

    course 2 lesson 10 homework

--- apps/DogBreeds/DogBreeds/View Controllers/MainViewController.swift
+++ apps/DogBreeds/DogBreeds/View Controllers/MainViewController.swift
@@ -38,22 +38,40 @@ class RootViewController: UITableViewController {
                 guard let subBreed = breed.subBreed else {continue}
                 url = URL(string: Url.randomImageWithSubBreed(
                     breed: breed.name,
-                    subBreed: subBreed).urlString)
-            
+                    subBreed: subBreed
+                ).urlString)
+                break
+            }
+            guard let url = url else { continue }
+            // TODO: move it to init of breed
+            NetworkManager.shared.fetchJson(
+                from: url,
+                responseType: APIResponse<String>.self
+            ) { result in
+                switch result {
+                case .success(let response):
+                    breed.randomPicture = response.message
+                case .failure(let error):
+                    print(error)
+                }
             }
         }
         
     }
     
+    // TODO: move it somewhree out of vc
     private func populateBreeds(from breedsDict: Dictionary<String, [String]>) {
         breeds = []
         for (breed, subBreeds) in breedsDict {
+            
+            
+            
             print(breed, subBreeds)
             if subBreeds.count == 0 {
-                self.breeds.append(Breed(name: breed, subBreed: nil, type: .breed))
+                self.breeds.append(Breed(name: breed, type: .breed, subBreed: nil))
             } else {
                 for subBreed in subBreeds {
-                    self.breeds.append(Breed(name: breed, subBreed: subBreed, type: .subBreed))
+                    self.breeds.append(Breed(name: breed, type: .subBreed, subBreed: subBreed))
                 }
             }
         }
