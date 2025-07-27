9f37acf9ccdc023acde925e90c2261277caf8ff6
Egor Egorov <egigoka@gmail.com>
2025-07-27 22:20:05 +0500

    course 2 lesson 10 homework

--- apps/DogBreeds/DogBreeds/Models/Response.swift
+++ apps/DogBreeds/DogBreeds/Models/Response.swift
@@ -5,6 +5,17 @@
 //  Created by egigoka on 27.07.2025.
 //
 
+enum Urls: String {
+    case listAllBreeds = "https://dog.ceo/api/breeds/list/all"
+    case randomImageByBreed(breed: String, subBreed: String?) -> String {
+        if let subBreed = subBreed {
+            return "https://dog.ceo/api/breed/\(breed)/\(subBreed)/images/random"
+        }
+    }
+    case randomImageByBreed = "https://dog.ceo/api/breed/%@/images/random"
+    case randomImageBySubBreed =
+}
+
 struct APIResponse<T: Decodable>: Decodable {
     let message: T
     let status: String
