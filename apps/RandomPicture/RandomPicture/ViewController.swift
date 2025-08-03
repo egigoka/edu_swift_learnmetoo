fe8004a0dd922f0a684522de0356855d8f1d4ce3
Egor Egorov <egigoka@gmail.com>
2025-08-03 22:37:19 +0500

    course network lesson 4 theory

---
+++ apps/RandomPicture/RandomPicture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPicture
+//
+//  Created by egigoka on 03.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
