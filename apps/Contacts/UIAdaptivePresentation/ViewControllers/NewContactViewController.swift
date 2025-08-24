2647848bfc24427261fc420bdcd3bef67b21bd9d
Egor Egorov <egigoka@gmail.com>
2025-08-24 22:22:35 +0500

    course 2 lesson 12 theory

---
+++ apps/Contacts/UIAdaptivePresentation/ViewControllers/NewContactViewController.swift
@@ -0,0 +1,57 @@
+//
+//  NewContactViewController.swift
+//  UIAdaptivePresentation
+//
+//  Created by Alexey Efimov on 04/10/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+protocol NewContactViewControllerDelegate {
+    func saveContact(_ contact: Contact)
+}
+
+class NewContactViewController: UIViewController {
+
+    @IBOutlet var doneButton: UIBarButtonItem!
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    
+    var delegate: NewContactViewControllerDelegate!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        firstNameTextField.addTarget(
+            self,
+            action: #selector(firstNameTextFieldDidChanged),
+            for: .editingChanged
+        )
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        saveAndExit()
+    }
+    
+    @IBAction func canceButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    @objc private func firstNameTextFieldDidChanged() {
+        guard let firstName = firstNameTextField.text else { return }
+        doneButton.isEnabled = !firstName.isEmpty ? true : false
+    }
+    
+    private func saveAndExit() {
+        guard let firstName = firstNameTextField.text else { return }
+        guard let lastName = lastNameTextField.text else { return }
+        
+        let contact = Contact(firstName: firstName, lastName: lastName)
+//        StorageManager.shared.save(contact: contact)
+        StorageManager.shared.saveToFile(with: contact)
+        
+        delegate.saveContact(contact)
+        dismiss(animated: true)
+    }
+}
