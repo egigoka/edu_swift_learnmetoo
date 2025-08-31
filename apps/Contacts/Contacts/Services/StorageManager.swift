22ceda20202f6ca31199bf07b56b80e35496ec6d
Egor Egorov <egigoka@gmail.com>
2025-09-01 00:43:09 +0500

    course 2 lesson 12 theory

--- apps/Contacts/Contacts/Services/StorageManager.swift
+++ apps/Contacts/Contacts/Services/StorageManager.swift
@@ -10,15 +10,24 @@ import Foundation
 class StorageManager {
     static let shared = StorageManager()
     
-    private init() {}
-    
     private let userDefaults = UserDefaults.standard
     private let contactKey = "contacts"
+    private let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    private var archiveUrl: URL
+    
+    private init() {
+        //print(documentDirectory)
+        archiveUrl = documentDirectory.appendingPathComponent("Contacts").appendingPathExtension("plist")
+    }
     
     func append(contact: Contact) {
         var contacts = fetchContacts()
         contacts.append(contact)
         save(contacts: contacts)
+    }
+    
+    func appendToFile(with contact: Contact) {
+        
     }
     
     func fetchContacts() -> [Contact] {
@@ -27,9 +36,20 @@ class StorageManager {
         return contacts
     }
     
+    func fetchContactsFromFile() -> [Contact] {
+        guard let data = try? Data(contentsOf: archiveUrl) else { return [] }
+        guard let contacts = try? PropertyListDecoder().decode([Contacts].self, from: data) else { return [] }
+        return contacts
+    }
+    
     func save(contacts: [Contact]) {
         guard let data = try? JSONEncoder().encode(contacts) else { return }
         userDefaults.set(data, forKey: contactKey)
+    }
+    
+    func saveToFile(contacts: [Contact]) {
+        guard let data = try? PropertyListEncoder().encode(contacts) else { return }
+        
     }
     
     func delete(at index: Int) {
