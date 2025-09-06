e319c2c6fc713868a95f4555f1e68f81e5d50f33
Egor Egorov <egigoka@gmail.com>
2025-09-06 23:14:26 +0500

    course 2 lesson 13 theory

--- apps/CoreDataDemo/CoreDataDemo/TaskListViewController.swift
+++ apps/CoreDataDemo/CoreDataDemo/TaskListViewController.swift
@@ -14,16 +14,29 @@ class TaskListViewController: UITableViewController {
         view.backgroundColor = .white
         setupNavigationBar()
         
-        let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.configureWithOpaqueBackground()
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
     }
     
     private func setupNavigationBar() {
         title = "Task list"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navBarAppearance.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
     }
 
 
 }
 
+ 
