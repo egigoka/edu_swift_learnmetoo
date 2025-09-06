de1bc2a41db0381b22e22563b7b0182ae131a290
Egor Egorov <egigoka@gmail.com>
2025-09-06 23:43:24 +0500

    course 2 lesson 13 theory

--- apps/CoreDataDemo/CoreDataDemo/NewTaskViewController.swift
+++ apps/CoreDataDemo/CoreDataDemo/NewTaskViewController.swift
@@ -15,23 +15,58 @@ class NewTaskViewController: UIViewController {
         textField.borderStyle = .roundedRect
         return textField
     }()
+    
+    private lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 1
+        )
+        button.setTitle("Save task", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(save), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupSubViews()
+        setupConstraints()
     }
     
     private func setupSubViews() {
-        setupConstraints()
         view.addSubview(taskTextField)
+        view.addSubview(saveButton)
     }
     
     private func setupConstraints() {
         taskTextField.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            
+            taskTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 32),
+            taskTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            taskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: taskTextField.topAnchor, constant: 64),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    @objc private func save() {
+        dismiss(animated: true)
+    }
+    
+    @objc private func cancel() {
+        dismiss(animated: true)
     }
 }
