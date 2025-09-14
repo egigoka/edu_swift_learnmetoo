3b4b28da4fab06a82e1b595016c86e0689a4c8e2
Egor Egorov <egigoka@gmail.com>
2025-09-14 13:45:06 +0500

    course 2 lesson 14 homework

--- apps/RealmDemo/RealmDemo/ViewControllers/TasksViewController.swift
+++ apps/RealmDemo/RealmDemo/ViewControllers/TasksViewController.swift
@@ -6,35 +6,53 @@
 //
 
 import UIKit
+import RealmSwift
 
 class TasksViewController: UITableViewController {
     
     var taskList: TaskList!
     
+    var currentTasks: Results<Task>!
+    var completedTasks: Results<Task>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = taskList.name
+        
+        currentTasks = taskList.tasks.filter("isComplete = false")
+        completedTasks = taskList.tasks.filter("isComplete = true")
     }
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        0
+        2
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        section == 0 ? currentTasks.count : completedTasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        section == 0 ? "CURRENT TASKS" : "COMPLETED TASKS"
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TasksCell", for: indexPath)
         
+        let task = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
         var content = cell.defaultContentConfiguration()
-        
-        //guard let task = taskList[indexPath.row] as? Task else { return cell }
-        //content.text = taskList[indexPath.row].name
-        
+        content.text = task.name
+        content.secondaryText = task.note
+        cell.contentConfiguration = content
         
         return cell
     }
     
+}
+
+extension TasksViewController {
+    
+    private func showAlert() {
+        
+    }
 }
