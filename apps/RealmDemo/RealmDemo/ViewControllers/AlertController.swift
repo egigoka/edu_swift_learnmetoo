17f0ed69150068ac85255960adbc33fb8338f9e8
Egor Egorov <egigoka@gmail.com>
2025-09-21 19:48:40 +0500

    course 2 lesson 14 homework

--- apps/RealmDemo/RealmDemo/ViewControllers/AlertController.swift
+++ apps/RealmDemo/RealmDemo/ViewControllers/AlertController.swift
@@ -21,24 +21,24 @@ class AlertController: UIAlertController {
     let taskList = object as? TaskList
     let isTask = task == nil
         
-        let title = object == nil ? "Add Task\(isTask ? "" : " List")" : "Edit Task\(isTask ? "" : " List")"
-        let message = object == nil ? "Enter a name for your new task\(isTask ? "" : " list")." : "Edit the name of your task\(isTask ? "" : " list")."
-        let buttonTitle = object == nil ? "Add" : "Save"
-        
-        let alert = UIAlertController(
-            title: title,
-            message: message,
-            preferredStyle: .alert)
-        
-        alert.addTextField { textField in
-            textField.placeholder = "Task\(isTask ? "" : " List") Name"
-            if let task = task {
-                textField.text = task.name
-            }
+    let title = object == nil ? "Add Task\(isTask ? "" : " List")" : "Edit Task\(isTask ? "" : " List")"
+    let message = object == nil ? "Enter a name for your new task\(isTask ? "" : " list")." : "Edit the name of your task\(isTask ? "" : " list")."
+    let buttonTitle = object == nil ? "Add" : "Save"
+    
+    let alert = UIAlertController(
+        title: title,
+        message: message,
+        preferredStyle: .alert)
+    
+    alert.addTextField { textField in
+        textField.placeholder = "Task\(isTask ? "" : " List") Name"
+        if let task = task {
+            textField.text = task.name
+        }
         if let taskList = taskList {
             textField.text = taskList.name
         }
-        }
+    }
         
     if isTask {
             alert.addTextField { TextField in
@@ -54,13 +54,22 @@ class AlertController: UIAlertController {
             guard let name = alert.textFields?.first?.text else { return }
             let note = alert.textFields?[1].text ?? ""
             var task = task
-            if task == nil {
-                task = StorageManager.shared.createNew(Task.self, value: ["name": name, "note": note])
-                guard let task = task, let taskListParent = taskListParent else { return }
-                StorageManager.shared.appendToList(taskListParent, to: "tasks", with: task)
-            } else {
-                StorageManager.shared.update(task, value: ["name": name, "note": note])
-            }
+        if isTask {
+                if task == nil {
+                    task = StorageManager.shared.createNew(Task.self, value: ["name": name, "note": note])
+                    guard let task = task, let taskListParent = taskListParent else { return }
+                    StorageManager.shared.appendToList(taskListParent, to: "tasks", with: task)
+                } else {
+                    StorageManager.shared.update(task, value: ["name": name, "note": note])
+                }
+        } else {
+            if taskList == nil {
+                    let _ = StorageManager.shared.createNew(TaskList.self, value: ["name": name])
+                } else {
+                    guard let taskList = taskList else { return }
+                    StorageManager.shared.update(taskList, value: ["name": name])
+                }
+        }
         }
         
         alert.addAction(cancelAction)
