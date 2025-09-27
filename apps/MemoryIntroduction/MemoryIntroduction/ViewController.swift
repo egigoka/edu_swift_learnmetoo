b90b586978e63e2fcdd79513d409910c157012bd
Egor Egorov <egigoka@gmail.com>
2025-09-28 00:44:13 +0500

    course 2 lesson 15 theory

---
+++ apps/MemoryIntroduction/MemoryIntroduction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryIntroduction
+//
+//  Created by egigoka on 28.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
