d33ae830e23dc59404c66d22febffba8aa0ee867
Egor Egorov <egigoka@gmail.com>
2025-09-28 22:27:04 +0500

    course 3 lesson 1 theory

---
+++ apps/SwiftUIApp/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by egigoka on 28.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
