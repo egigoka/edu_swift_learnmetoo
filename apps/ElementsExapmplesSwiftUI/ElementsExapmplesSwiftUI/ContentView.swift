59c8750b1b2fd5e479f1148c732ca6f50310bc97
Egor Egorov <egigoka@gmail.com>
2025-10-05 16:47:17 +0500

    course 3 lesson 2 theory

--- apps/ElementsExapmplesSwiftUI/ElementsExapmplesSwiftUI/ContentView.swift
+++ apps/ElementsExapmplesSwiftUI/ElementsExapmplesSwiftUI/ContentView.swift
@@ -8,15 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue = Double.random(in: 0...1)
+    
     var body: some View {
+        
         VStack {
-            Text("Hello, world!")
+            VStack {
+                Text("\(sliderValue)")
+                    .font(.largeTitle)
+                
+                .padding(.horizontal)
+                ColorSlider(value: $sliderValue)
+                ColorSlider(value: $sliderValue)
+                ColorSlider(value: $sliderValue)
+                Spacer()
+            }
             
         }
         .padding()
+        
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    
+    var body: some View {
+        HStack {
+            Text("0")
+                .foregroundColor(.red)
+            Slider(value: $value)
+            Text("255")
+                .foregroundColor(.red)
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
