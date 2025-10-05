9c05705e62e604f420a92e23aef042459100b493
Egor Egorov <egigoka@gmail.com>
2025-10-05 17:35:51 +0500

    course 3 lesson 2 theory

--- apps/ElementsExapmplesSwiftUI/ElementsExapmplesSwiftUI/ContentView.swift
+++ apps/ElementsExapmplesSwiftUI/ElementsExapmplesSwiftUI/ContentView.swift
@@ -10,11 +10,6 @@ import SwiftUI
 struct ContentView: View {
     @State private var sliderValue = Double(Int.random(in: 0...255))
     
-    //debug
-    @State private var speed = 50.0
-    @State private var isEditing = false
-    //debug END
-    
     var body: some View {
         
         VStack {
@@ -27,26 +22,6 @@ struct ContentView: View {
                 ColorSlider(value: $sliderValue, textColor: .green)
                 ColorSlider(value: $sliderValue, textColor: .blue)
                 Spacer()
-                
-                //debug
-                Slider(
-                    value: $speed,
-                    in: 0...100,
-                    step: 5
-                ){
-                    
-                            Text("Speed")
-                        } minimumValueLabel: {
-                            Text("0")
-                        } maximumValueLabel: {
-                            Text("100")
-                        
-                } onEditingChanged: { editing in
-                    isEditing = editing
-                }
-                Text("\(speed)")
-                    .foregroundColor(isEditing ? .red : .blue)
-                //debug END
             }
             
         }
@@ -63,7 +38,7 @@ struct ColorSlider: View {
         HStack {
             Text("0")
                 .foregroundColor(textColor)
-            Slider(value: $value, in: 0...255, step: 1) {}
+            Slider(value: $value, in: 0...10, step: 1) {} // empty closure to use different init of Slider to workaround bug that doesn't apply step argument
             Text("255")
                 .foregroundColor(textColor)
         }
