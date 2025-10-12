7e539941bccdebff8cb1e2aea655aec311193578
Egor Egorov <egigoka@gmail.com>
2025-10-12 20:09:06 +0500

    course 3 lesson 3 theory

---
+++ apps/StateAndDataFlowApp/StateAndDataFlowApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  StateAndDataFlowApp
+//
+//  Created by egigoka on 12.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apps/StateAndDataFlowApp/StateAndDataFlowApp/StateAndDataFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowAppApp.swift
+//  StateAndDataFlowApp
+//
+//  Created by egigoka on 12.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
