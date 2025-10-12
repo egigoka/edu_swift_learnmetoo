d85300fff6ce450a114f9d1f092597d33e83d353
Egor Egorov <egigoka@gmail.com>
2025-10-12 20:28:32 +0500

    course 3 lesson 3 theory

--- apps/StateAndDataFlowApp/StateAndDataFlowApp/ContentView.swift
+++ apps/StateAndDataFlowApp/StateAndDataFlowApp/ContentView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .padding()
+            Text("\(tapCount)")
+                .font(.largeTitle)
+                .offset(x: 0, y: 100)
+            Spacer()
+            Button("Tap me") {
+                tapCount += 1
+            }
+            Spacer()
         }
         .padding()
     }
