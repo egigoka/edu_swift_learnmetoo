f8478d84398a0acd50384cfd49e7462239b6c30e
Egor Egorov <egigoka@gmail.com>
2025-10-19 15:02:26 +0500

    course 3 lesson 4 homework review

--- apps/StateAndDataFlowApp/StateAndDataFlowApp/UserManager.swift
+++ apps/StateAndDataFlowApp/StateAndDataFlowApp/UserManager.swift
@@ -5,9 +5,19 @@
 //  Created by egigoka on 19.10.2025.
 //
 
+import SwiftUI
 import Combine
 
 class UserManager: ObservableObject {
-    @Published var isRegistered = false
-    @Published var name = ""
+    @AppStorage("isRegistered") var isRegistered = false {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    @AppStorage("userName") var name = "" {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    @Published
 }
