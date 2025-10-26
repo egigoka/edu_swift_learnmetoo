2bfba8f2a40c513233df97df449aaf181be60f86
Egor Egorov <egigoka@gmail.com>
2025-10-27 02:58:03 +0500

    course 3 lesson 4 theory

---
+++ apps/SwiftUIColorizedApp/SwiftUIColorizedApp/ColorUISlider.swift
@@ -0,0 +1,23 @@
+//
+//  ColorUISlider.swift
+//  SwiftUIColorizedApp
+//
+//  Created by egigoka on 27.10.2025.
+//  Copyright © 2025 Alexey Efimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ColorUISlider: UIViewRepresentable {
+    func makeUIView(context: Context) -> UISlider {
+        <#code#>
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        <#code#>
+    }
+}
+
+#Preview {
+    ColorUISlider()
+}
