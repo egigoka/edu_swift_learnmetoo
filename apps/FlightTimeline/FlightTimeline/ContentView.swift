15eb0fc9b2127e75e996cc353c0d60e04d9c6282
Egor Egorov <egigoka@gmail.com>
2025-10-27 03:27:47 +0500

    course 3 lesson 4 theory

---
+++ apps/FlightTimeline/FlightTimeline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlightTimeline
+//
+//  Created by egigoka on 27.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
