c23612edcbe0225de427c6380026f7339946effc
Egor Egorov <egigoka@gmail.com>
2025-11-02 03:49:04 +0500

    course 3 lesson 4 homework

--- apps/SliderGame/SliderGame/UISliderView.swift
+++ apps/SliderGame/SliderGame/UISliderView.swift
@@ -11,16 +11,41 @@ struct UISliderView: UIViewControllerRepresentable {
     
     @Binding var currentValue: Float
     let targetValue: Int
+    let coordinator: Coordinator
+    
+    init(currentValue: Binding<Float>, targetValue: Int) {
+        self._currentValue = currentValue
+        self.targetValue = targetValue
+        self.coordinator = Coordinator(currentValue: currentValue)
+    }
+    
+    class Coordinator {
+        @Binding var currentValue: Float
+        
+        init(currentValue: Binding<Float>) {
+            self._currentValue = currentValue
+        }
+        
+        @objc func onSliderChange(_ value: Float) {
+            currentValue = value
+        }
+    }
     
     func makeUIViewController(context: Context) -> some UIViewController {
+        
         let viewController = UIViewController()
         let slider = UISlider()
         viewController.view.addSubview(slider)
         
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: slider, attribute: .leading, relatedBy: .equal, toItem: viewController, attribute: .leading),
+            NSLayoutConstraint(item: slider, attribute: .trailing, relatedBy: .equal, toItem: viewController, attribute: .trailing)
+        ])
+        
         slider.maximumValue = 100
         slider.value = currentValue
         slider.thumbTintColor = .red
-        slider.addTarget(self, action: #selector(onSliderChange), for: .valueChanged)
+        slider.addTarget(self, action: #selector(coordinator.onSliderChange), for: .valueChanged)
         
         
         return viewController
@@ -31,10 +56,9 @@ struct UISliderView: UIViewControllerRepresentable {
         
     }
     
-    @objc private func onSliderChange(_ value: Float) {
-        currentValue = value
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(currentValue: $currentValue)
     }
-    
 }
 
 #Preview {
