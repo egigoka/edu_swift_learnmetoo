07892dfe9aeb5336c9c9f93ff9a7a47a136bd525
Egor Egorov <egigoka@gmail.com>
2025-11-02 00:01:35 +0500

    course 3 lesson 4 theory

--- apps/FlightTimeline/FlightTimeline/Models/TimelineInfo.swift
+++ apps/FlightTimeline/FlightTimeline/Models/TimelineInfo.swift
@@ -16,56 +16,35 @@ struct TimelineInfo: UIViewControllerRepresentable {
         print("\n\n\ntest, date: \(Date())")
         let controller = UITableViewController()
         
-//        let bundle = Bundle(for: TimelineTableViewCell.self)
-//        let bundle = Bundle.main //debug
         let bundle = Bundle(for: TimelineTableViewCell.self)
-        print("bundle: \(bundle)")
-        DispatchQueue.main.async {
-            
-            if let nibUrl = bundle.url(forResource: "TimelineTableViewCell", withExtension: "bundle") {
-                print("nibUrl \(nibUrl)")
-            } else {
-                print("no nib url")
-            }
-            //        let nibBundle = Bundle(url: nibUrl)
-            //        let nib = UINib(nibName: "TimelineTableViewCell", bundle: nibBundle)
-            //
-            //        controller.tableView.register(nib, forCellReuseIdentifier: "TimelineTableViewCell")
-            //
-            //        controller.tableView.dataSource = context.coordinator
-            
+        
+        guard let nibUrl = bundle.url(
+            forResource: "TimelineTableViewCell_TimelineTableViewCell",
+            withExtension: "bundle"),
+              let nibBundle = Bundle(url: nibUrl) else
+        {
+            print("Could not load TimelineTableViewCell bundle")
+            return controller
         }
+        print("Loaded bundle: \(nibBundle)")
+        
+        let nib = UINib(nibName: "TimelineTableViewCell", bundle: nibBundle)
+        controller.tableView.register(nib, forCellReuseIdentifier: "TimelineTableViewCell")
+        print("Registered successfully")
+        
+        controller.tableView.dataSource = context.coordinator
+        
         return controller
     }
     
     func updateUIViewController(_ uiViewController: UITableViewController,
                                 context: Context) {
-        print("\n uiViewController.tableView.dataSource \(String(describing: uiViewController.tableView.dataSource))")
-        print("controller.tableView.dataSource is Coordinator \(uiViewController.tableView.dataSource is Coordinator)")
-        
-        
-        if !(uiViewController.tableView.dataSource is Coordinator) {
-            let bundle = Bundle(for: TimelineTableViewCell.self)
-            
-            if let nibUrl = bundle.url(forResource: "TimelineTableViewCell", withExtension: "bundle") {
-                print("nib url: \(nibUrl)")
-            } else {
-                print("no nib url")
-            }
-            
-//            let nibBundle = Bundle(url: nibUrl)!
-//            let nib = UINib(nibName: "TimelineTableViewCell", bundle: nibBundle)
-            
-//            uiViewController.tableView.register(nib, forCellReuseIdentifier: "TimelineTableViewCell")
-            
-            uiViewController.tableView.dataSource = context.coordinator
-        } else {
-            uiViewController.tableView.reloadData()
-        }
+        uiViewController.tableView.reloadData()
     }
     
     func makeCoordinator() -> Coordinator {
-        Coordinator(flights: flights)
+        print("registering coordinator")
+        return Coordinator(flights: flights)
     }
 }
 
@@ -94,8 +73,12 @@ extension Coordinator: UITableViewDataSource {
         let scheduledString = dateFormatter.string(from: flight.scheduledTime) // screduled flights
         let currentString = dateFormatter.string(from: flight.currentTime ?? flight.scheduledTime) // current flights
         
+        print("dequequeing the cell")
         let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineTableViewCell", for: indexPath)
-        guard let cell = cell as? TimelineTableViewCell else { return cell }
+        print("got cell")
+        guard let cell = cell as? TimelineTableViewCell else {
+            return cell
+        }
         
         var flightInfo = "\(flight.airline) \(flight.number) "
         flightInfo += "\(flight.direction == .departure ? "to" : "from")"
