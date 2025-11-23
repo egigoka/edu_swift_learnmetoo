20db2ba81c9e961a644a9f5811c7b59602f734ef
Egor Egorov <egigoka@gmail.com>
2025-11-23 19:42:01 +0500

    course 3 lesson 6 theory

--- apps/AwardsCollectionApp/AwardsCollectionApp/Views/PathView.swift
+++ apps/AwardsCollectionApp/AwardsCollectionApp/Views/PathView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct PathView: View {
+    var width: CGFloat
+    var height: CGFloat
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            let size = min(geometry.size.height, geometry.size.width)
+            let nearLine = size * 0.1
+            let farLine = size * 0.9
+            let middle = size / 2
+            
+            Path { path in
+                path.move(to: CGPoint(x: middle + nearLine, y: nearLine))
+                path.addLine(to: CGPoint(x: farLine, y: farLine))
+                path.addLine(to: CGPoint(x: nearLine, y: farLine))
+                path.addLine(to: CGPoint(x: middle - nearLine, y: nearLine))
+            }
+            .fill(Color(red: 0.4, green: 0.4, blue: 0.4))
+            
+            Path { path in
+                path.move(to: CGPoint(x: middle, y: farLine))
+                path.addLine(to: CGPoint(x: middle, y: nearLine))
+            }
+            .stroke(
+                Color.white,
+                style: StrokeStyle(lineWidth: 3,
+                                   dash: [geometry.size.height / 20])
+            )
+            
+            Image(systemName: "car.fill")
+                .resizable()
+                .foregroundStyle(Color.white)
+                .scaleEffect(0.2)
+                .offset(x: -geometry.size.width / 7.25)
+        }
+        .frame(width: 200, height: 200)
     }
 }
 
 #Preview {
-    PathView()
+    PathView(width: 200, height: 200)
 }
