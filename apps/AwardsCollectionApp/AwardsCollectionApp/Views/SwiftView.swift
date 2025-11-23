483d6047ef51be387597b2ad3b07bb3b10395bb3
Egor Egorov <egigoka@gmail.com>
2025-11-24 00:28:12 +0500

    course 3 lesson 6 theory

--- apps/AwardsCollectionApp/AwardsCollectionApp/Views/SwiftView.swift
+++ apps/AwardsCollectionApp/AwardsCollectionApp/Views/SwiftView.swift
@@ -20,20 +20,21 @@ struct SwiftView: View {
                 path.addQuadCurve(to: point(x: 315, y: 286), control: point(x: 325, y: 245))
                 path.addQuadCurve(to: point(x: 250, y: 267), control: point(x: 275, y: 248))
                 path.addQuadCurve(to: point(x: 12, y: 193), control: point(x: 135, y: 335))
-                path.addQuadCurve(to: point(x: 182, y: 212), control: point(x: 110, y:250))
+                path.addQuadCurve(to: point(x: 182, y: 212), control: point(x: 110, y: 250))
                 path.addQuadCurve(to: point(x: 40, y: 55), control: point(x: 90, y: 130))
                 path.addQuadCurve(to: point(x: 158, y: 146), control: point(x: 50, y: 70))
                 path.addQuadCurve(to: point(x: 76, y: 39), control: point(x: 115, y: 100))
                 path.addQuadCurve(to: point(x: 226, y: 160), control: point(x: 180, y: 130))
                 path.addQuadCurve(to: point(x: 207, y: 13), control: point(x: 260, y: 100))
             }
+            .fill()
             .stroke(.black, lineWidth: 2)
         }
         .frame(width: width, height: height)
     }
     
     private func point(x: CGFloat, y: CGFloat) -> CGPoint {
-        return CGPoint(x: x * width / 256 , y: y * height / 256)
+        return CGPoint(x: x, y: y)
     }
 }
 
