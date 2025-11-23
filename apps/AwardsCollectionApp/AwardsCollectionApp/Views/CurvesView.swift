b19434d0f1960588b393b5e2d8a8bf2a2d6248aa
Egor Egorov <egigoka@gmail.com>
2025-11-23 20:01:54 +0500

    course 3 lesson 6 theory

--- apps/AwardsCollectionApp/AwardsCollectionApp/Views/CurvesView.swift
+++ apps/AwardsCollectionApp/AwardsCollectionApp/Views/CurvesView.swift
@@ -18,8 +18,34 @@ struct CurvesView: View {
             let farLine = size * 0.9
             let middle = size / 2
             
-            Path
+            Path { path in
+                path.move(to: CGPoint(x: middle, y: nearLine))
+                path.addQuadCurve(
+                    to: CGPoint(x: farLine, y: middle),
+                    control: CGPoint(x: size, y: 0)
+                )
+                path.addQuadCurve(
+                    to: CGPoint(x: middle, y: farLine),
+                    control: CGPoint(x: size, y: size)
+                )
+                path.addQuadCurve(
+                    to: CGPoint(x: nearLine, y: middle),
+                    control: CGPoint(x: 0, y: size)
+                )
+                path.addQuadCurve(
+                    to: CGPoint(x: middle, y: nearLine),
+                    control: CGPoint(x: 0, y: 0)
+                )
+            }
+            .fill(
+                RadialGradient(
+                    colors: [.white, .yellow],
+                    center: .center,
+                    startRadius: size * 0.05,
+                    endRadius: size * 0.6)
+            )
         }
+        .frame(width: width, height: height)
     }
 }
 
