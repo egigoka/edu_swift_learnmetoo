597344de91aff79de9349cbc890150d9145431f1
Egor Egorov <egigoka@gmail.com>
2025-11-23 21:06:12 +0500

    course 3 lesson 6 theory

--- apps/AwardsCollectionApp/AwardsCollectionApp/Views/ContentView.swift
+++ apps/AwardsCollectionApp/AwardsCollectionApp/Views/ContentView.swift
@@ -10,40 +10,38 @@ import SwiftUI
 struct ContentView: View {
     @State private var showShape = false
     private let shapeSize: CGFloat = 250
-    @State private var screenWidth: CGFloat = 0
     
     var body: some View {
         GeometryReader{ geometry in
-            let _ = self.screenWidth = geometry.size.width
-            let _ = print(self.screenWidth)
-            Circle()
-        }
-        .ignoresSafeArea()
-        VStack {
-            Button(action: { showShape.toggle() }) {
-                HStack {
-                    Text(showShape ? "Hide Shape" : "Show Shape")
-                    Spacer()
-                    Image(systemName: "chevron.up.square")
-                        .rotationEffect(.degrees(showShape ? 0 : 180))
-                        .animation(.default, value: 0.2)
+            VStack {
+                Button(action: { showShape.toggle() }) {
+                    HStack {
+                        Text(showShape ? "Hide Shape" : "Show Shape")
+                        Spacer()
+                        Image(systemName: "chevron.up.square")
+                            .rotationEffect(.degrees(showShape ? 0 : 180))
+                            .animation(.default, value: showShape)
+                    }
                 }
+                
+                Spacer()
+                
+                GradientRectangles(width: shapeSize, height: shapeSize)
+                    .offset(
+                        x: showShape
+                        ? 0
+                        : -geometry.size.width / 2
+                        - geometry.safeAreaInsets.leading
+                        - shapeSize / 2
+                    )
+                    .animation(.default, value: showShape)
+                
+                Spacer()
             }
-            
-            Spacer()
-            
-            GradientRectangles(width: shapeSize, height: shapeSize)
-                .offset(
-                    x: showShape ? 0 : -UIScreen.main.bounds.width / 2 - shapeSize / 2
-                )
-                .offset(x: showShape ? 0 : CGFloat(screenWidth / 2) + shapeSize / 2)
-                .animation(.default, value: 0.2)
-            
-            
-            Spacer()
+            .font(.headline)
+            .padding()
+            .ignoresSafeArea(edges: .horizontal)
         }
-        .font(.headline)
-        .padding()
     }
 }
 
