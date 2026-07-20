cdc363a9e5b20090a5df36766e8199ac065bc599
egigoka <egigoka@gmail.com>
2026-07-21 00:29:52 +0500

    course 3 lesson 1 theory

--- apps/TrafficLightSwiftUI/TrafficLightSwiftUI/ColoredCircle.swift
+++ apps/TrafficLightSwiftUI/TrafficLightSwiftUI/ColoredCircle.swift
@@ -13,18 +13,7 @@ struct ColoredCircle: View {
     let opacity: Double
     
     var body: some View {
-//        Color(color)
-//            //.frame(width: 100, height: 100)
-//            .scaledToFit()
-//            .clipShape(Circle())
-//            .opacity(opacity)
-//            .overlay(Circle().stroke(Color(.label), lineWidth: 4))
-        Circle()
-            .foregroundStyle(color)
-            .scaledToFit()
-            .opacity(opacity)
-            .overlay(Circle().stroke(Color(.label),
-                                    lineWidth: 4))
+        
     }
 }
 
--- apps/TrafficLightSwiftUI/TrafficLightSwiftUI/ContentView.swift
+++ apps/TrafficLightSwiftUI/TrafficLightSwiftUI/ContentView.swift
@@ -9,25 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var buttonText = "START"
-    @State private var currentLight: CurrentLight = .none
-    
     var body: some View {
-        VStack {
-            VStack {
-                ColoredCircle(
-                    color: .red, opacity: currentLight == .red ? 1 : 0.3)
-                ColoredCircle(
-                    color: .yellow, opacity: currentLight == .yellow ? 1 : 0.3)
-                ColoredCircle(
-                    color: .green, opacity: currentLight == .green ? 1 : 0.3)
-            }
-        }
-        .padding(.top)
-        ResponsiveSpacer(portraitHeight: 60, landscapeHeight: 0)
-        StylizedButton(title: buttonText) {
-            buttonTapped()
-        }
+        
     }
     
 }
@@ -35,13 +18,7 @@ struct ContentView: View {
 extension ContentView {
     
     private func buttonTapped() {
-        switch currentLight {
-        case .none: currentLight = .red
-        case .red: currentLight = .yellow
-        case .yellow: currentLight = .green
-        case .green: currentLight = .red
-        }
-        buttonText = "NEXT"
+        
     }
 }
 
