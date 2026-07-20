67f46a24d6af235a97fb96a36c8aa3b42dcdc7eb
egigoka <egigoka@gmail.com>
2026-07-20 23:25:14 +0500

    course 3 lesson 1 theory

--- apps/SwiftUIApp/SwiftUIApp/ContentView.swift
+++ apps/SwiftUIApp/SwiftUIApp/ContentView.swift
@@ -24,6 +24,9 @@ struct ContentView: View {
             }
             .padding()
             Spacer()
+            Button {
+                "Button"
+            }
         }
         .padding()
     }
