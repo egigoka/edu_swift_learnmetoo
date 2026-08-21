9d6bfb8321319308ed7240cd365289e94931b3c8
egigoka <egigoka@gmail.com>
2026-08-21 19:38:58 +0500

    course 3 lesson 3 theory

--- apps/StateAndDataFlowApp/StateAndDataFlowApp/ContentView.swift
+++ apps/StateAndDataFlowApp/StateAndDataFlowApp/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tapCount = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(tapCount)")
+                .font(.largeTitle)
+                .offset(x: 0, y: 100)
+            Spacer()
+            ButtonView(tapCount: $tapCount, buttonColor: .red)
+            Spacer()
         }
         .padding()
+    }
+}
+
+struct ButtonView: View {
+    
+    @Binding var tapCount: Int
+    
+    let buttonColor: Color
+    
+    var body: some View {
+        Button("Tap me!") {
+            tapCount += 1
+        }
+        Button {
+            tapCount += 1
+        } label: {
+            Text
+        }
+
     }
 }
 
