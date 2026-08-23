f5549c90e971df46f8179993e9bd4c0065f132e2
egigoka <egigoka@gmail.com>
2026-08-23 15:05:37 +0500

    course 3 lesson 4 homework review

--- apps/StateAndDataFlowApp/StateAndDataFlowApp/DataManager.swift
+++ apps/StateAndDataFlowApp/StateAndDataFlowApp/DataManager.swift
@@ -5,30 +5,31 @@
 //  Created by egigoka2kz on 22.08.2026.
 //
 
-import Foundation
+import SwiftUI
 
 class DataManager {
+    
     static let shared = DataManager()
     
-    private let userDefaults = UserDefaults.standard
-    private let userDataKey = "userManager"
+    @AppStorage("user") private var userData = Data()
     
     private init() {}
     
     func saveUser(user: User) {
         guard let userData = try? JSONEncoder().encode(user) else { return }
-        userDefaults.set(userData, forKey: userDataKey)
+        self.userData = userData
     }
     
     func loadUser() -> User {
-        guard let userData = userDefaults.object(forKey: userDataKey) as? Data else { return User() }
-        guard let user = try? JSONDecoder().decode(User.self, from: userData) else { return User() }
+        guard let user = try? JSONDecoder().decode(User.self, from: userData) else {
+            return User()
+        }
         return user
     }
     
     func clear(userManager: UserManager) {
         userManager.user.isRegistered = false
         userManager.user.name = ""
-        userDefaults.removeObject(forKey: userDataKey)
+        UserDefaults.standard.removeObject(forKey: "user")
     }
 }
