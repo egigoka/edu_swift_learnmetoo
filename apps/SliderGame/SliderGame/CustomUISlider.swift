dd647d2c0c9b8e459c0221fb37453255e0f5f30b
egigoka <egigoka@gmail.com>
2026-08-30 21:20:44 +0500

    course 3 lesson 4 homework

---
+++ apps/SliderGame/SliderGame/CustomUISlider.swift
@@ -0,0 +1,56 @@
+//
+//  SwiftUIView.swift
+//  SliderGame
+//
+//  Created by egigoka2kz on 30.08.2026.
+//
+
+import SwiftUI
+
+struct CustomUISlider: UIViewRepresentable {
+    
+    @Binding var value: Double
+    let color: UIColor
+    let alpha: CGFloat
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.thumbTintColor = color
+        slider.alpha = alpha
+        
+        slider.addTarget(context.coordinator, action: #selector(Coordinator.valueChanged), for: .valueChanged)
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = Float(value)
+    }
+    
+    func makeCoordinator() -> CustomUISlider.Coordinator {
+        Coordinator(value: $value)
+    }
+    
+    //typealias UIViewType = UISlider
+}
+
+extension CustomUISlider {
+    class Coordinator: NSObject {
+        @Binding var value: Double
+        
+        init(value: Binding<Double>) {
+            self._value = value
+        }
+        
+        @objc func valueChanged(_ sender: UISlider) {
+            value = Double(sender.value)
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var value = 0.0
+    CustomUISlider(value: $value, color: .red, alpha: 0.5)
+}
---
+++ apps/SliderGame/SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by egigoka2kz on 30.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
