2429008479fdc8d78a90b112a215ae0b8388bdea
egigoka <egigoka@gmail.com>
2026-09-07 20:49:56 +0500

    course 3 lesson 8 homework review

---
+++ apps/SwiftUIiOS14/SwiftUIiOS14/SwiftUIiOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIiOS14App.swift
+//  SwiftUIiOS14
+//
+//  Created by egigoka2kz on 07.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIiOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello")
+        }
+    }
+}
