9719efa8f7cea1ac76b0c43cea592457f987616d
egigoka <egigoka@gmail.com>
2026-09-22 15:00:18 +0500

    course 4 lesson 4 theory

--- apps/SwiftBookSwiftUIMVVM/SwiftBookSwiftUIMVVM/Views/CourseImage.swift
+++ apps/SwiftBookSwiftUIMVVM/SwiftBookSwiftUIMVVM/Views/CourseImage.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 struct CourseImage: View {
     let imageData: Data?
+    let imageSize: CGSize
+    let cornerRadius: CGFloat
+    let shadowIsOn: Bool
     
     var body: some View {
         getImage(from: imageData)
+            .resizable()
+            .frame(width: imageSize.width, height: imageSize.height)
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+            .shadow(radius: shadowIsOn ? 10 : 0)
     }
     
     private func getImage(from data: Data?) -> Image {
@@ -24,5 +31,10 @@ struct CourseImage: View {
 }
 
 #Preview {
-    CourseImage(imageData: nil)
+    CourseImage(
+        imageData: nil,
+        imageSize: CGSize(width: 100, height: 100),
+        cornerRadius: 10,
+        shadowIsOn: true
+    )
 }
